82591b7aa16accfda3dc1171eb635cf157306139
= <=>
2019-04-24 20:43:33 +0700

    Updates

---
+++ precache-manifest.80cba74ff0d247984fe184363590ae5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f723c245561a796e31cec42b8118da2",
+    "url": "/salas-loading-images/index.html"
+  },
+  {
+    "revision": "dfb29ef2c1a39512d812",
+    "url": "/salas-loading-images/static/css/main.5ff54ccb.chunk.css"
+  },
+  {
+    "revision": "9eab2023acdf4af01e97",
+    "url": "/salas-loading-images/static/js/2.a8fc8d78.chunk.js"
+  },
+  {
+    "revision": "dfb29ef2c1a39512d812",
+    "url": "/salas-loading-images/static/js/main.c67199cc.chunk.js"
+  },
+  {
+    "revision": "ac135670c14977819d9d",
+    "url": "/salas-loading-images/static/js/runtime~main.9bd52a3c.js"
+  }
+]);
